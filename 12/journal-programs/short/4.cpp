d7c5ba27b91c1d964309bd9e607c769b40c3c12a
HarshKhandeparkar <mjkgoa@gmail.com>
2021-03-05 12:58:34 +0530

    feat<journal_programs>: add short#4

---
+++ 12/journal-programs/short/4.cpp
@@ -0,0 +1,42 @@
+/**
+ * STATEMENT: Write a C++ program to count the number of vowels in a given line of text. Output
+ * the line of text and the number of vowels in it.
+ */
+
+#include <iostream>
+#include <ctype.h>
+#include <string.h>
+
+using namespace std;
+
+class CountVowels {
+  private:
+    const char vowels[5] = { 'a', 'e', 'i', 'o', 'u' };
+    bool isVowel(char c) {
+      c = tolower(c);
+
+      for (int i = 0; i < 5; i++) if (vowels[i] == c) return true;
+      return false;
+    }
+  public:
+    CountVowels() {
+      string str;
+      int numVowels = 0;
+
+      cout << "Enter the string: ";
+      getline(cin, str);
+
+      for (int i = 0; i < str.length(); i++) {
+        numVowels += isVowel(str[i]);
+      }
+
+      cout << "String: " << str << endl;
+      cout << "Vowels: " << numVowels << endl;
+    }
+};
+
+int main() {
+  CountVowels V;
+
+  return 0;
+}
