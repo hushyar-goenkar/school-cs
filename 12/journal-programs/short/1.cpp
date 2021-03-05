ca6d1497dc53e9c7f518d2d35b066c58c2a54a61
HarshKhandeparkar <mjkgoa@gmail.com>
2021-03-05 12:35:22 +0530

    feat<journal_programs>: add short#1

---
+++ 12/journal-programs/short/1.cpp
@@ -0,0 +1,31 @@
+/**
+ * STATEMENT: Write a C++ program to reverse a given positive integer M,
+ * of arbitrary length(i.e. any number of digits) and output M as well as its reverse.
+ */
+
+#include <iostream>
+using namespace std;
+
+class ReverseNumber {
+  public:
+    ReverseNumber() {
+      int no, rev = 0;
+
+      cout << "Enter Number: ";
+      cin >> no;
+
+      while (no > 0) {
+        rev *= 10;
+        rev += no % 10;
+        no /= 10;
+      }
+
+      cout << "Reversed Number: " << rev << endl;
+    }
+};
+
+int main() {
+  ReverseNumber N;
+
+  return 0;
+}
