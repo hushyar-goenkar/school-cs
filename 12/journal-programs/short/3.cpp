fae4b4535ae4f9f00d2af16b4aee2bd40840ed94
HarshKhandeparkar <mjkgoa@gmail.com>
2021-03-05 12:49:53 +0530

    feat<journal_programs>: add short#3

---
+++ 12/journal-programs/short/3.cpp
@@ -0,0 +1,33 @@
+/**
+ * STATEMENT: Write a C++ program to generate the first N terms of the Fibonacci series
+ * 0, 1, 1, 2, 3, 5...
+ */
+
+#include <iostream>
+using namespace std;
+
+class Fibonacci {
+  public:
+    Fibonacci() {
+      int n; // Number of fibonacci terms to display; n to be entered by the user
+      int lastFibTerm = 1; // The last term in this sequence (considering the term before 1 to be 0)
+      int fibSum = 0; // The current sum of all terms ie the new term in the sequence (The sum of first 1 term is 1)
+
+      cout << "Enter The Number of Terms: ";
+      cin >> n;
+
+      for (int i = 0; i < n; i++) {
+        cout << fibSum << " "; // Print the sum ie the current term
+        fibSum += lastFibTerm; // Add the last term to the sum of the current digit to get the next term
+
+        lastFibTerm = fibSum - lastFibTerm; // The last fibsum is the new last term
+      }
+      cout << endl;
+    }
+};
+
+int main() {
+  Fibonacci F;
+
+  return 0;
+}
